207c55005c7d0cd0eaf15815d2612e6c66d1ff2c
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-01-11 17:12:15 +0100

    LockBoxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''LockBoxes interview'''
+
+def canUnlockAll(boxes):
+    '''Determinate if all boxes are openable'''
+    if not boxes:
+        return False
+
+    keys = set([0])
+    opened = set()
+
+    while keys:
+        current_key = keys.pop()
+        opened.add(current_key)
+
+        for key in boxes[current_key]:
+            if key < len(boxes) and key not in opened:
+                keys.add(key)
+
+    return len(opened) == len(boxes)
+
+if __name__ == "__main__":
+    boxes1 = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes1))
+
+    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes2))
+
+    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes3))
