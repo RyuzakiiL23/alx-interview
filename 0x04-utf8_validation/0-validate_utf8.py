6c7b783a23b5fd4fdd0e00f959d3502a609ea8dc
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-02-03 12:40:02 +0100

    UTF8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,24 +15,43 @@ def validUTF8(data):
         bool: True if data is a valid UTF-8 encoding, else return False.
     """
 
-    num_bytes = 0
+    number_bytes = 0
 
-    for num in data:
-        if 128 <= num <= 191:
-            if num_bytes == 0:
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
-            num_bytes -= 1
+
         else:
-            if num_bytes == 0:
-                if 192 <= num <= 223:
-                    num_bytes = 1
-                elif 224 <= num <= 239:
-                    num_bytes = 2
-                elif 240 <= num <= 247:
-                    num_bytes = 3
-                elif num >= 248:
-                    return False
-            else:
+            if not (i & mask_1 and not (i & mask_2)):
                 return False
 
-    return num_bytes == 0
+        number_bytes -= 1
+
+    return number_bytes == 0
+
+
+if __name__ == "__main__":
+    data1 = [65]
+    print(validUTF8(data1))
+
+    data2 = [80, 121, 116, 104, 111, 110, 32, 105,
+             115, 32, 99, 111, 111, 108, 33]
+    print(validUTF8(data2))
+
+    data3 = [229, 65, 127, 256]
+    print(validUTF8(data3))
