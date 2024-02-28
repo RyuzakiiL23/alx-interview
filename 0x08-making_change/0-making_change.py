137c0b8d4d78bf5cf71841795ca6a87ee6a3ed3f
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2024-02-28 15:40:25 +0100

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module for making change problem
+"""
+
+
+def makeChange(coins, total):
+    """
+    Returns the fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+
+if __name__ == "__main__":
+    print(makeChange([1, 2, 25], 37))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
